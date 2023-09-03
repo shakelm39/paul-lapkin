f44096630dc9bc9920d1e1baf514b08e626ac199
Shakel Kawsar <62901201+shakelm39@users.noreply.github.com>
2023-09-03 11:19:26 +0600

    paul-lapkin

---
+++ resources/js/main.js
@@ -0,0 +1,15 @@
+// mixitup js 
+var mixer = mixitup('.work-grid');
+
+//wow js 
+var wow = new WOW().init();
+
+//menu 
+
+function menuOpen(){
+    document.getElementById('menu-container').style.width ='100%';
+    
+}
+function menuClose(){
+    document.getElementById('menu-container').style.width ='0';
+}
